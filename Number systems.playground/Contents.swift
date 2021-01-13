9d0fd574cadef8d492eb6f295ecb4b78be9432d0
CharleySun <Ysun@lcs.on.ca>
2021-01-13 12:02:39 -0500

    Got to functions changed to convert binary and octal

--- Number systems.playground/Contents.swift
+++ Number systems.playground/Contents.swift
@@ -25,6 +25,11 @@ var str = "Hello, playground"
 1 / 2
 
 
+
+// Enumeration
+//
+// An enumeration simply lists 
+
 // Functions
 //
 // Functions are a way to group (encapsulate) related behaviour
@@ -39,36 +44,45 @@ var str = "Hello, playground"
  
  */
 
-// Get the binary representation of a value
-func getBinaryRepresentation(of valueToConvert: Int) -> String {
+// Get the alternate representation of a value in a different base
+func getRepresentation(of valueToConvert: Int, inBase base: Int) -> String {
  
     // Create a variable with the value of "valueToConvert"
     // A variable can be changed once created
     var decimalValueLeftToConvert = valueToConvert
     // This creates an empty string
     // A string is just text, like "hello"
-    var binaryRepresentation = ""
+    var representation = ""
 
     //The abstraction we will use is a LOOP
     // Our END CONDITION is that thedecimalValueLeftToConvert is equal to zero
     // So long as the CONDITION is true, the block of code surrounded by the  { } brackets will be run repeatedly
     while decimalValueLeftToConvert > 0 {
        
-        // Get the next binary digit
-        let nextBinaryDigit = decimalValueLeftToConvert % 2
+        // Get the next digit
+        let nextDigit = decimalValueLeftToConvert % base
         
         // Add the new digit to the binary representation
         // Swift is a strictly typed language
         // It does not automatically convert data types
         // So, to make the Int into a String, we need to specify this
-        binaryRepresentation = String(nextBinaryDigit) + binaryRepresentation
+        representation = String(nextDigit) + representation
         
         // Get the decimal value left to convert
-        decimalValueLeftToConvert = decimalValueLeftToConvert / 2
+        decimalValueLeftToConvert = decimalValueLeftToConvert / base
+        
     }
-   return binaryRepresentation
+   return representation
 
 }
 
 // Call, or use the function
-getBinaryRepresentation(of: 17)
+getRepresentation(of: 17, inBase: 2)
+getRepresentation(of: 17, inBase: 8)
+getRepresentation(of: 17, inBase: 16)
+
+// 1 * 16 ^1 + 1 * 16^0 + 16 + 1 = 17
+
+getRepresentation(of: 15, inBase: 16)
+
+getRepresentation(of: 14, inBase: 7)
